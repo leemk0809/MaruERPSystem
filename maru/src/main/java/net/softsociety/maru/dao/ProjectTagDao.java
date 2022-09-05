bcedac59e4a8d4520b644c2f8b7d688e37fe5d34
MOMO <MOMO@TFX5550UC>
2022-09-05 14:43:01 +0900

    정훈

---
+++ maru/src/main/java/net/softsociety/maru/dao/ProjectTagDao.java
@@ -0,0 +1,21 @@
+package net.softsociety.maru.dao;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import net.softsociety.maru.domain.ProjectTag;
+
+@Mapper
+public interface ProjectTagDao {
+
+	public int insert(ProjectTag projectTag);
+	
+	public int delete(int project_tag_num);
+	
+	public int update(ProjectTag projectTag);
+	
+	public ProjectTag selectOne(int project_tag_num);
+	
+	public ArrayList<ProjectTag> selectAll();
+}
