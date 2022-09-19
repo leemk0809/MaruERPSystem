3eb3bd57b0014ba6c7f1abb2319e4a34e4016826
sproutish0201 <sproutish0201@gmail.com>
2022-09-19 17:28:01 +0900

    혜인 (Funds 분기별 수입/지출 그래프 DB연결)

--- maru/src/main/java/net/softsociety/maru/controller/FundsController.java
+++ maru/src/main/java/net/softsociety/maru/controller/FundsController.java
@@ -1,5 +1,7 @@
 package net.softsociety.maru.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,15 +25,39 @@ public class FundsController {
 		int proSales1 = service.projectSales(1);
 		int proSales2 = service.projectSales(2);
 		int proSales3 = service.projectSales(3);
-		int proSales4 = service.projectSales(4);
-		
+		int proSales4 = service.projectSales(4);		
 		model.addAttribute("proSales1",proSales1);
 		model.addAttribute("proSales2",proSales2);
 		model.addAttribute("proSales3",proSales3);
 		model.addAttribute("proSales4",proSales4);
 		
 		
+		//분기별 수입/지출 지표		
+		ArrayList<Integer> qsList22= service.quarterlySales(2022);
+		int qs221=qsList22.get(0);
+		int qs222=qsList22.get(1);
+		int qs223=qsList22.get(2);
+		int qs224=qsList22.get(3);
+		model.addAttribute("qs221",qs221);
+		model.addAttribute("qs222",qs222);
+		model.addAttribute("qs223",qs223);
+		model.addAttribute("qs224",qs224);
+		ArrayList<Integer> qeList22 = service.quarterlyExpense(2022);
+		model.addAttribute("qeList22",qeList22);
+		int qe221=qeList22.get(0);		
+		int qe222=qeList22.get(1);
+		int qe223=qeList22.get(2);
+		int qe224=qeList22.get(3);
+		model.addAttribute("qe221",qe221);
+		model.addAttribute("qe222",qe222);
+		model.addAttribute("qe223",qe223);
+		model.addAttribute("qe224",qe224);
 		
 		return "manage/funds";
+	}
+
+	private void alert(ArrayList<Integer> qeList22) {
+		// TODO Auto-generated method stub
+		
 	}
 }
