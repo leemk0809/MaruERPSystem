0a0d528d17a9544c19acba5d030b72478de84ee2
MOMO <MOMO@TFX5550UC>
2022-09-21 17:47:15 +0900

    서정훈 사인디테일 니드매트리얼 서비스 만들어서 콘트롤러 올려라

---
+++ maru/src/main/java/net/softsociety/maru/controller/SignDetailController.java
@@ -0,0 +1,36 @@
+package net.softsociety.maru.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+import net.softsociety.maru.domain.Projects;
+import net.softsociety.maru.service.project.ProjectInfoService;
+
+@Slf4j
+@Controller
+public class SignDetailController {
+
+	@Autowired
+	ProjectInfoService service;
+	
+	@Autowired
+
+
+	@GetMapping("dashboard/signDetail")
+	public String signDetail(Model model , int projects_num) {
+		
+		Projects projects = service.selectOne(projects_num);
+		
+		//ArrayList<NeedMaterial> nMlist
+
+		model.addAttribute("projects", projects);
+		
+		return "dashboard/signDetail.html";
+	}
+
+}
