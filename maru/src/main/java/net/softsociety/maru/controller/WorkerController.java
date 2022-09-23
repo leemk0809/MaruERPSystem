d28dfc4850ae776096f5a87e9cc4413ef242acd1
sproutish0201 <sproutish0201@gmail.com>
2022-09-23 14:37:14 +0900

    혜인 (Worker insert)

--- maru/src/main/java/net/softsociety/maru/controller/WorkerController.java
+++ maru/src/main/java/net/softsociety/maru/controller/WorkerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -39,7 +40,20 @@ public class WorkerController {
 	@ResponseBody
 	@GetMapping("/delete")
 	public void delete(int num) {
-		
+		//이거 cascade 때문에 삭제 안됨. 테이블 엎어야됨
 		//service.deleteWorker(num);
 	}
+	
+	
+	@ResponseBody
+	@PostMapping("/insertWorker")
+	public List<Worker> insertWorker(Worker worker){
+		log.debug("worker : {}",worker);
+		service.insertWorker(worker);
+		
+		
+		return service.selectAll();
+	}
+	
+	
 }
