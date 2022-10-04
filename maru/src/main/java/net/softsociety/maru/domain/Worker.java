a60f417d95ed0693dcd72bc94a53879798b8b932
MOMO <MOMO@TFX5550UC>
2022-10-04 13:59:50 +0900

    서정훈 woker 도메인

--- maru/src/main/java/net/softsociety/maru/domain/Worker.java
+++ maru/src/main/java/net/softsociety/maru/domain/Worker.java
@@ -1,5 +1,10 @@
 package net.softsociety.maru.domain;
 
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +12,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Worker {
+public class Worker implements UserDetails {
 
+	private static final long serialVersionUID = -8700644315335148281L;
+	
 	private int worker_num;
 	private int division_num;
 	private int position_num;
@@ -17,5 +24,48 @@ public class Worker {
 	private String name;
 	private String phone;
 	private int salary;
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return this.id;
+	}
+	
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return this.password;
+	}
+	
+	public int getPosition_num() {
+		// TODO Auto-generated method stub
+		return this.position_num;
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 }
